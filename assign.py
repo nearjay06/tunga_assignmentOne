757e6f4e8f8c81618c876a593fab860ce6f6508f
nearjay06 <nearjoan2018@gmail.com>
2019-08-16 15:54:49 +0300

    made corrections to python assignment one

--- assign.py
+++ assign.py
@@ -1,49 +1,85 @@
-# Question One
-# Write a Python program which accepts the radius 
-# and height from the user and computes the volume
-# of a cylinder. Formula is: 3.14 x radius x radius x height 
+# # Question One
+
+radius = float(input("Enter the radius"))
+height = float(input("Enter the height"))
+
+volume = 3.14*(radius**2)*height
+print(volume)
+
 def calc_volume():
-    vol = 3.14*radius*radius*height
+    radius = float(input("Enter the radius"))
+    height = float(input("Enter the height"))
+    return 3.14*(radius**2)*height
+
+print(calc_volume())
+
+def calVolume(radius,height):
+    return 3.14*(radius**2)*height
+print (calVolume(radius = float(input("Enter the radius"), height = float(input("Enter the height")))))
+
+# def calc_volume():
+#     vol = 3.14*radius*radius*height
     
-    pi = 3.14 
-    c = input("Enter radius here: ")
-    d = input("Enter height here: ")
+#     pi = 3.14 
+#     c = input("Enter radius here: ")
+#     d = input("Enter height here: ")
 
-    volume = f"{'pi'}*{'c'} * {'c'}* {'d'}
-    print ("volume is:", volume)
-
-
+#     volume = f"{'pi'}*{'c'} * {'c'}* {'d'}
+#     print ("volume is:", volume)
 
 # Question Two
-a = [2,3,4,5,6,7]
-print (a[:0] + a[3:])
+myList = [5,6,8,9,5,32]
+for index in range(len(myList)):
+    if len(myList)> 1:
+        removedItem = myList.pop(1)
+        print(removedItem)
 
-
+# a = [2,3,4,5,6,7]
+# print (a[:0] + a[3:])
 
 
 #Question three
 
-s = ['a','b','c','d']
-for items in s:
-    if items == len(s):
-        print (s)
-    else:
-        print ('invalid position')
+name = 'abcd'
+for i in range(len(name)):
+    print (1, name[i])
 
+# s = ['a','b','c','d']
+# for items in s:
+#     if items == len(s):
+#         print (s)
+#     else:
+#         print ('invalid position')  
 
 #Question four
-input = [1,2,3,4,5,6,7]
-print (sum(input))
+
+total = 0
+for item in myList:
+    total = total + item
+
+# input = [1,2,3,4,5,6,7]
+# print (sum(input))
 
 
 #Question five
-def largest_number(b):
-    b = (1,2,3,4,5,6)
-    for items in b:
-        if b >1 and b < 8:
-            print (b)
-        else:
-            print('invalid')
+def largest(*numbers):
+    largeNum = 0
+    for item in numbers:
+        if item > largeNum:
+            largeNum = item
+
+    return largeNum
+
+print(largest(23,67,889,999,4))
+
+# def largest_number(b):
+#     b = (1,2,3,4,5,6)
+#     for items in b:
+#         if b >1 and b < 8:
+#             print (b)
+#         else:
+#             print('invalid')
+
 
 
 
